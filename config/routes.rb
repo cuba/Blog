282006e8cfaf3b0b7375f028b1dde619d22d4111
Jacob Sikorski <jacob.sikorski@gmail.com>
2012-06-02 18:06:16 -0400

    Modified signup page
    - Added routes
    - Added unit tests
    - Added title

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ SampleApp::Application.routes.draw do
   match '/help' => 'static_pages#help'
   match '/about' => 'static_pages#about'
   match '/contact' => 'static_pages#contact'
+  match '/signup' => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
