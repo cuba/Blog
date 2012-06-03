beb0b0c9c10a957b3a3c336d2dfa47351556658d
Jacob Sikorski <jacob.sikorski@gmail.com>
2012-06-03 00:28:57 -0400

    Created Form
    - Including unit test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,19 @@ class UsersController < ApplicationController
   end
   
   def new
-    #new_user = User.new()
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if (@user.save)
+      #handle successful save
+    else
+      render 'new'
+    end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "User Pages" do
   subject { page }
 
-  describe "signup page" do
+  describe "Signup page" do
     before { visit signup_path }
     it { should have_selector('h1', :text => 'Sign up')}
     it { should have_selector('title', :text => full_title('Sign up'))}
@@ -14,5 +14,31 @@ describe "User Pages" do
     before { visit user_path(user) }
     it { should have_selector('h1', :text => user.username) }
     it { should have_selector('title', :text => user.username) }
+  end
+
+  describe "signup" do
+    before {visit signup_path }
+    let(:submit) { "Create my Account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect {click_button submit}.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Username",               :with => "Test.User"
+        fill_in "Email",                  :with => "Test.User@example.com"
+        fill_in "Firstname",             :with => "Test"
+        fill_in "Lastname",              :with => "User"
+        fill_in "Password",               :with => "foobar"
+        fill_in "Password confirmation",  :with => "foobar" 
+      end
+
+      it "should create a user" do
+        expect {click_button submit }.to change(User, :count).by(1) 
+      end
+    end
   end
 end
