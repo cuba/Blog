43f68bfa4cab4d6c4aeb0af94e2eabd5505ab85a
Jacob Sikorski <jacob.sikorski@gmail.com>
2012-11-25 17:44:36 -0500

    fixed resizing of elements smaller than 350px

--- app/assets/javascripts/responsive-layout.js
+++ app/assets/javascripts/responsive-layout.js
@@ -12,6 +12,13 @@ window.onresize = function(event) {
     });
 };
 
+window.onload = function(event) {
+    jQuery(".kubee-layout-row").each(function(){
+        var row = jQuery(this);
+        resizeCells(row, 360);
+    });
+};
+
 function resizeCells(row, minSize){
     var cells = row.children(".kubee-layout-cell");
     var numberOfCells = cells.length;
@@ -35,7 +42,7 @@ function resizeCells(row, minSize){
             cell.removeClass("kubee-layout-cell-33");
             cell.removeClass("kubee-layout-cell-100");
         }
-        else if(row.width() > minSize && row.width() < minSize*2 ){
+        else if(row.width() < minSize*2 ){
             cell.addClass("kubee-layout-cell-100");
             cell.removeClass("kubee-layout-cell-25");
             cell.removeClass("kubee-layout-cell-33");
